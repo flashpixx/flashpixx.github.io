d63d0d2fce27d687fdaa6cfeed8a0c9e149ed4bc
Philipp Kraus <philipp.kraus@flashpixx.de>
2016-12-28 03:25:13 +0100

    fix naming, add page jump

--- themes/flashpixx/static/script.js
+++ themes/flashpixx/static/script.js
@@ -1,10 +1,15 @@
+"use strict";
+
 jQuery(function() {
 
-    jQuery.scrollify({
-        section : ".content"
-    });
+    jQuery.scrollify({ section : ".content" });
 
     jQuery(".menulink").click( function() {
         jQuery.scrollify.move( jQuery(this).attr("href") );
     });
+
+    var l_path = window.location.pathname.replace(/\//g, "");
+    if (l_path)
+        jQuery.scrollify.instantMove( "#page_" + l_path );
+
 });

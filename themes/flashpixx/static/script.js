4684bb42c6d5a54009faf4596f885305cca918d7
Philipp Kraus <philipp.kraus@flashpixx.de>
2016-12-29 20:25:38 +0100

    disable touch

--- themes/flashpixx/static/script.js
+++ themes/flashpixx/static/script.js
@@ -2,7 +2,7 @@
 
 jQuery(function() {
 
-    jQuery.scrollify({ section : ".content" });
+    jQuery.scrollify({ section : ".content", touchScroll: false });
 
     jQuery(".menulink").click( function() {
         jQuery.scrollify.move( jQuery(this).attr("href") );
